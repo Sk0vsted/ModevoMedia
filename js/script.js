f79476de50440d74f6b9b81e8b595bfd2148ea78
Andreas Skovsted <113526100+Sk0vsted@users.noreply.github.com>
2023-03-06 11:20:14 +0100

    Changed Code
    Tilføjet About.html side og lav forholdsvis mange ændringer i index.html, style.scss og script.js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,36 @@
+const navSlide =() => {
+  const burger = document.querySelector('#burger');
+  const nav = document.querySelector('.nav-links');
+  const navLinks = document.querySelectorAll('.nav-links li');
+  const anchorlinks = document.querySelectorAll('.nav-links li a')
+  
+  burger.addEventListener('click', () => {
+      nav.classList.toggle('nav-active');
+
+      navLinks.forEach((link, index) => {
+          if(link.style.animation){
+              link.style.animation = '';
+          } else {
+              link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.7}s`;
+
+              link.style.animation = `navLinkUnderline 0.5s ease forwards ${index/7+0.7}s`;
+          }
+      });
+
+      anchorlinks.forEach((link, index) => {
+        if (link.style.animation) {
+            link.style.animation = '';
+        } else {
+            link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.7}s`;
+        }
+    });
+
+      //Burger Animation
+      burger.classList.toggle('toggle');
+  });
+}
+navSlide();
+
 const dots = Array.from(document.querySelectorAll(".dot"));
 
 let slideIndex = 1;
@@ -32,3 +65,37 @@ function showSlides(n) {
   dots[slideIndex - 1].classList.add("is-active");
 }
 dots.forEach((dot) => dot.addEventListener("click", currentSlide));
+
+
+
+    // Get references to all the image elements
+    var teamImages = document.getElementsByClassName("member-img");
+
+    // Loop through the images and bind the changeImage function to the mouseover and mouseout events
+    for (var i = 0; i < teamImages.length; i++) {
+        teamImages[i].addEventListener("mouseover", function() {
+            changeImage(this, this.src.replace("-1.jpg", "-2.jpg"));
+        });
+        teamImages[i].addEventListener("mouseout", function() {
+            changeImage(this, this.src.replace("-2.jpg", "-1.jpg"));
+        });
+    }
+
+    function changeImage(element, newSrc) {
+        // Create a new image object
+        var img = new Image();
+
+        // When the new image is loaded, set the src attribute of the element
+        img.onload = function() {
+          setTimeout(function() {
+            element.src = newSrc;
+            element.style.opacity = 1;
+          }, 100);
+        }
+
+        // Set the src attribute of the new image object to the new source
+        img.src = newSrc;
+
+        // Fade out the current image by setting its opacity to 0
+        element.style.opacity = 0;
+    }
